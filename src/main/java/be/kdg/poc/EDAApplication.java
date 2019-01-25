dba1fa8b80760dae9089ccfcbc15e016c531f7f7
Cédric Goffin <goffin.cedric@hotmail.com>
2019-01-25 19:55:50 +0100

    Initial commit

---
+++ src/main/java/be/kdg/poc/EDAApplication.java
@@ -0,0 +1,11 @@
+package be.kdg.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDAApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EDAApplication.class, args);
+    }
+}
